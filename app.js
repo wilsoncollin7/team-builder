c526a110dbfecc29f43649992796a614fe97450a
wilsoncollin7 <wilsoncollin7@gmail.com>
2020-08-31 11:29:13 -0400

    all javascript is working and the generator is up and running

--- app.js
+++ app.js
@@ -10,26 +10,100 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+// empty array waiting for the team to be pushed
+const team = [];
 
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
+// the questions for the user
+const employeeQuestions = [
+    {
+        type: "input",
+        name: "name",
+        message: "What is the employee's name?",
+        validate: (value) => {
+            if (value === "" || value === null) {
+                return "Employee name cannot be empty."
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is the employee's ID nubmer?",
+        validate: (value) => {
+            if (value === "" || value === null) {
+                return "Employee ID cannot be empty."
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is the Employee's email?",
+        validate: (value) => {
+            if (value === "" || value === null) {
+                return "Employee email cannot be empty."
+            } else {
+                return true;
+            }
+        }
+    },
+    {
+        type: "list",
+        name: "role",
+        message: "What is the Employee's role?",
+        choices: ["manager", "engineer", "intern"]
+    }
+];
 
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
+// the function the filters the employee based on the role, sent to classes
+employeeFunc = (name, id, email, role) => {
+    if(role === "manager") {
+        return new Manager(name, id, email, "");
+    } else if (role === "engineer") {
+        return new Engineer(name, id, email, "");
+    } else {
+        return new Intern(name, id, email, "");
+    }
+};
 
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
+// write to file function that creates the file
+writeToFile = (fileName, data) => {
+    fs.writeFile(fileName, data, (err) => {
+        console.log(err);
+    })
+    console.log("Team made successfully!")
+}
 
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
+// the main team making function that askes the questions and then pushes the output to the team array
+makeTeam = async() => {
+    let addEmployee = true
+    console.log("Team builder running! Answer following questions:");
+    while(addEmployee){
+        await inquirer.prompt(employeeQuestions).then(async answers => {
+            let employee = employeeFunc(answers.name, answers.id, answers.email, answers.role);
+            await employee.roleBasedQuestions();
+            team.push(employee);
+        });
 
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
+        await inquirer.prompt(
+            {
+                type: "confirm",
+                name: "addEmployee",
+                message: "Would you like to add another employee?"
+            }
+        ).then(answers => {
+            addEmployee = answers.addEmployee;
+        });
+    }
+
+    const output = render(team);
+    writeToFile(outputPath, output);
+}
+
+//initiate the main function of the app
+makeTeam();
+
